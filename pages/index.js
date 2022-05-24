84198125ef9349d4e46ad2230b8d808a4123e396
Matilde Park <m@park.computer>
2022-05-24 01:01:10 -0400

    index: use logo

--- pages/index.js
+++ pages/index.js
@@ -54,8 +54,12 @@ export default function Home() {
         />
       </Head>
 
-      <h2 className="text-center text-4xl z-10 bg-black p-2 fixed">
-        Reciprocal is an interactive design and development studio.
+      <h2 className="text-center flex items-center text-4xl z-10 bg-black pr-2 fixed">
+        <img
+          className="w-24 pr-4"
+          src="https://s3.us-east-1.amazonaws.com/haddefsigwen1/reciprocal/2022.5.20..19.52.50-B137D4AA-DA9E-4B07-98CE-C001C576C7CA_1_201_a.jpeg"
+        />{" "}
+        is an interactive design and development studio.
       </h2>
 
       <Footer black />
