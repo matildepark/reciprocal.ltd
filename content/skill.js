d00760c788844c3e68a0f38f281188e36d66242a
Matilde Park <m@park.computer>
2022-05-25 16:11:00 -0700

    skill: more info

--- content/skill.js
+++ content/skill.js
@@ -9,16 +9,33 @@ Related material:
 Pressure and Protolith (presentation)
 
 <iframe width="300" height="115" src="https://www.youtube.com/embed/XjIDZQUZZtE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    `,
+`,
   },
-  { name: "front-end development", content: "" },
-  { name: "video game development", content: "" },
-  { name: "music and soundtrack production", content: "" },
-  { name: "art collage", content: "" },
+  {
+    name: "front-end development",
+    content: `We've collectively shipped front-end applications to production for over four years in nascent or experimental environments. We prefer React and Next.js, but work with anything. We also maintain a [fork of the Pleroma front-end](https://decept.org) as part of our fediverse work.`,
+  },
+  {
+    name: "video game development",
+    content: `MP ran [Aether Interactive](https://aether.in.net) making increasingly bizarre constructions in Twine for two years.
+
+Subserial Network famously networked seven Twine sessions through Electron's IPC as a pseudo-window manager experience.
+
+SI's worked in LOVE2D and made Doom mods (see that section).
+
+PJ and SI both create and curate Roblox experiences -- see [Fire Blox with Me](https://www.instagram.com/fire_blox_with_me/).`,
+  },
+  {
+    name: "music and soundtrack production",
+    content: `SI has self-published [seven albums](https://brainfunnel.bandcamp.com) and done the sound design on all her game mods.`,
+  },
+  {
+    name: "art collage",
+    content:
+      "PJ got everyone else into collages. This site uses one on the homepage -- so does MP's website.",
+  },
   { name: "Tezos", content: "" },
-  { name: "APL", content: "" },
-  { name: "J", content: "" },
-  { name: "K", content: "" },
+  { name: "APL + J + K", content: "" },
   { name: "Chialisp", content: "" },
   { name: "Postscript", content: "" },
   { name: "security consulting and research", content: "" },
@@ -27,9 +44,25 @@ Pressure and Protolith (presentation)
   { name: "p2p and blockchain systems", content: "" },
   { name: "wolves", content: "" },
   { name: "software archeology", content: "" },
-  { name: "Doom modding", content: "" },
-  { name: "esoteric programming languages", content: "" },
-  { name: "machine learning", content: "" },
+  {
+    name: "Doom modding",
+    content: `SI has been making Doom maps and mods since 2015 -- MP and SI met through connections in the Doom modder community. If you have some need of understanding differences across the various engines (or in the Source engine for that matter)...
+
+Related material:
+
+<iframe width="300" height="115" src="https://www.youtube.com/embed/ob_tr4fVZuM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<iframe width="300" height="115" src="https://www.youtube.com/embed/wi9NC1btyIk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+  },
+  {
+    name: "esoteric programming languages",
+    content: `SI wrote [Brainfuck in Lua](https://github.com/sink666/nutoad) for fun, why not.`,
+  },
+  {
+    name: "machine learning",
+    content:
+      "We have an external consult for ML and can incorporate it into necessary work, but it isn't our current specialisation.",
+  },
 ];
 
 export default skill;
