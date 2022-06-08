ff6fae7261ba35c277460b8465d58373ca600b25
Matilde Park <m@park.computer>
2022-06-07 18:58:08 -0600

    index: hide img on mobile

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
       <p className="text-8xl mt-8 md:hidden z-10 text-black font-sans">ꜜ</p>
       <div className="inplace text-center flex fixed">
         <img
-          className="w-24 mix-blend-darken"
+          className="w-24 mix-blend-darken hidden md:flex"
           src="https://s3.us-east-1.amazonaws.com/haddefsigwen1/reciprocal/2022.5.20..19.52.50-B137D4AA-DA9E-4B07-98CE-C001C576C7CA_1_201_a.jpeg"
         />
         <span className="text-center pl-4 hidden md:flex items-center text-4xl z-10 bg-black">
